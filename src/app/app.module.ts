4a10036a6b57451bf7d303ddc02155220686cf50
DanielRT96 <reti.daniel96@gmail.com>
2020-10-29 15:36:44 +0100

    Components + navBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { RegistrationComponent } from './main/registration/registration.component';
+import { SnackbarComponent } from './main/snackbar/snackbar.component';
+import { RateComponent } from './main/rate/rate.component';
+import { MainComponent } from './main/main.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegistrationComponent,
+    SnackbarComponent,
+    RateComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatInputModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
